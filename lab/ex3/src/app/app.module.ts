be1cf75faafdafb0676af44c10c015b4429f7ed5
kob-e <kobieshka@gmail.com>
2017-03-18 22:27:55 +0200

    Exercise 3 - services

---
+++ lab/ex3/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+
+import { DataService } from './data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    MenuItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
